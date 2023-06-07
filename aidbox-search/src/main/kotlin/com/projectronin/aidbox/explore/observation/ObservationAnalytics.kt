dd55649914eb4d80379403711a5689f84556009c
roninbrad <brad@projectronin.com>
2023-06-07 12:41:54 -0700

    Aidbox Client - simplify observation vitals example

--- aidbox-search/src/main/kotlin/com/projectronin/aidbox/explore/observation/ObservationAnalytics.kt
+++ aidbox-search/src/main/kotlin/com/projectronin/aidbox/explore/observation/ObservationAnalytics.kt
@@ -1,6 +1,5 @@
 package com.projectronin.aidbox.explore.observation
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.projectronin.aidbox.client.AidboxFhirClient
 import com.projectronin.aidbox.client.AidboxFhirSearchRequest
 import org.hl7.fhir.r4.model.Observation
@@ -14,25 +13,13 @@ class ObservationAnalytics {
             fields =  "code",
             count = 500,
             timeout = 60,
-           extraParams = mapOf("category" to "vital-signs")
+            extraParams = mapOf("category" to "vital-signs")
         )
 
         val observationResultsList : List<Observation> = client.queryResources(req)
 
-        // convert into a map  (where key is the code.text value)
-        //   multiple observations will have the same code.text, so this map will be much smaller size than the results list
-        val customCodeableMap = observationResultsList
-            .map { it.code!! }
-            .associate { it.text to it.coding.orEmpty().map { c -> PrintableCoding(c.display.orEmpty(), c.code.orEmpty(), c.system.orEmpty()) } }
-            .toSortedMap()
-
-        val textKeysOnly = customCodeableMap.keys.sorted()
-        println("MAP KEYS...")
-        println(textKeysOnly)
-
-        println("FULL MAP...")
-        val prettyJson = ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(customCodeableMap)
-        println(prettyJson)
+        // Group all records by code type  (i.e. "Temp", "BP", "Pulse", "Resp", etc)
+        val vitalsMap: Map<String, List<Observation>> = observationResultsList.groupBy { it.code!!.text }
     }
 
     private data class PrintableCoding(val display: String, val code: String, val system: String)
